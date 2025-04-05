9531b5305eebcaedc4734e1abd09368e098ad80a
hetfield74 <hetfield@merz-it-service.de>
2025-04-05 13:04:31 +0200

    Initial commit

---
+++ shoproot/lang/english/admin/paypal_module.php
@@ -0,0 +1,97 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: paypal_module.php 16400 2025-04-02 15:29:16Z GTB $
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+
+$lang_array = array(
+  'TEXT_PAYPAL_MODULE_HEADING_TITLE' => 'PayPal Products',
+  
+  'TABLE_HEADING_MODULES' => 'Module',
+  'TABLE_HEADING_FILENAME' => 'Module name (for internal usage)',
+  'TABLE_HEADING_SORT_ORDER' => 'Sorting',
+  'TABLE_HEADING_STATUS' => 'Status',
+  'TABLE_HEADING_ACTION' => 'Action',
+
+  'TABLE_HEADING_WALL_STATUS' => 'Display at Paymentwall',
+  'TABLE_HEADING_WALL_DESCRIPTION' => 'Description',
+  
+  'TEXT_PAYPAL_MODULE_PROFILE' => 'Profile',
+  'TEXT_PAYPAL_NO_PROFILE' => 'no Webprofile',
+  'TEXT_PAYPAL_STANDARD_PROFILE' => 'Standard Webprofile',
+  
+  'TEXT_PAYPAL_MODULE_LINK_SUCCESS' => 'Link at checkout',
+  'TEXT_PAYPAL_MODULE_LINK_SUCCESS_INFO' => 'Shall the payment link be displayed in the checkout?',
+
+  'TEXT_PAYPAL_MODULE_LINK_ACCOUNT' => 'Link at account',
+  'TEXT_PAYPAL_MODULE_LINK_ACCOUNT_INFO' => 'Shall the payment link be displayed in the account?',
+
+  'TEXT_PAYPAL_MODULE_PRODUCT' => 'Button at product',
+  'TEXT_PAYPAL_MODULE_PRODUCT_INFO' => 'Shall the PayPal button be displayed in the product details?',
+
+  'TEXT_PAYPAL_MODULE_CART_BNPL' => 'BNPL Button in cart',
+  'TEXT_PAYPAL_MODULE_CART_BNPL_INFO' => 'Shall the PayPal button be displayed in the shopping cart?',
+
+  'TEXT_PAYPAL_MODULE_PRODUCT_BNPL' => 'BNPL Button at product',
+  'TEXT_PAYPAL_MODULE_PRODUCT_BNPL_INFO' => 'Shall the PayPal button be displayed in the product details?',
+
+  'TEXT_PAYPAL_MODULE_CHECKOUT_BNPL' => 'BNPL Button in checkout',
+  'TEXT_PAYPAL_MODULE_CHECKOUT_BNPL_INFO' => 'Shall the PayPal button be displayed in the checkout?',
+
+  'TEXT_PAYPAL_MODULE_BOX_CART' => 'Button in box cart',
+  'TEXT_PAYPAL_MODULE_BOX_CART_INFO' => 'Shall the PayPal button be displayed in the pbox cart?',
+
+  'TEXT_PAYPAL_MODULE_BOX_CART_BNPL' => 'BNPL Button in box cart',
+  'TEXT_PAYPAL_MODULE_BOX_CART_BNPL_INFO' => 'Shall the PayPal button be displayed in the box cart?',
+
+  'TEXT_PAYPAL_MODULE_SAVE_PAYMENT' => 'Save payment method in the checkout (PayPal Vault)',
+  'TEXT_PAYPAL_MODULE_SAVE_PAYMENT_INFO' => 'Should the payment method used for PayPal be saved for a faster checkout for another order?',
+
+  'TEXT_PAYPAL_MODULE_OFFER_SAVE_PAYMENT' => 'Allow saved payment method (PayPal Vault)',
+  'TEXT_PAYPAL_MODULE_OFFER_SAVE_PAYMENT_INFO' => 'Allow a saved payment method for PayPal for a faster checkout?',
+
+  'TEXT_PAYPAL_MODULE_ACDC_EXTEND_CARDS' => 'Allow Creditcards without 3D Secure',
+  'TEXT_PAYPAL_MODULE_ACDC_EXTEND_CARDS_INFO' => 'There is no liability shift without 3D Secure.',
+
+  'TEXT_PAYPAL_MODULE_USE_TABS' => 'Accordion / Tabs',
+  'TEXT_PAYPAL_MODULE_USE_TABS_INFO' => 'Does the template use accordion or tabs in the checkout?',
+
+  'TEXT_PAYPAL_MODULE_SHIPPING_COST' => 'provisional shipping costs',
+  'TEXT_PAYPAL_MODULE_SHIPPING_COST_INFO' => 'Amount for provisional shipping costs.',
+  
+  'TEXT_PAYPAL_MODULE_UPSTREAM_PRODUCT' => 'Show at product',
+  'TEXT_PAYPAL_MODULE_UPSTREAM_PRODUCT_INFO' => 'Show details for Installment at product?',
+
+  'TEXT_PAYPAL_MODULE_UPSTREAM_CART' => 'Show at cart',
+  'TEXT_PAYPAL_MODULE_UPSTREAM_CART_INFO' => 'Show details for Installment at cart?',
+
+  'TEXT_PAYPAL_MODULE_UPSTREAM_PAYMENT' => 'Show at checkout',
+  'TEXT_PAYPAL_MODULE_UPSTREAM_PAYMENT_INFO' => 'Show details for Installment during checkout?',
+
+  'TEXT_PAYPAL_BUTTON_LAYOUT' => 'Button Layout',
+  'TEXT_PAYPAL_BUTTON_LAYOUT_INFO' => 'Select the button layout',
+
+  'TEXT_PAYPAL_BUTTON_SHAPE' => 'Button Shape',
+  'TEXT_PAYPAL_BUTTON_SHAPE_INFO' => 'Select the button shape',
+
+  'TEXT_PAYPAL_BUTTON_PRIMARY_COLOR' => 'Button Color Paypal',
+  'TEXT_PAYPAL_BUTTON_PRIMARY_COLOR_INFO' => 'Select the button color',
+
+  'TEXT_PAYPAL_BUTTON_SECONDARY_COLOR' => 'Button Color BNPL',
+  'TEXT_PAYPAL_BUTTON_SECONDARY_COLOR_INFO' => 'Select the button color',
+
+  'TEXT_PAYPAL_BUTTON_HEIGHT' => 'Button Height',
+  'TEXT_PAYPAL_BUTTON_HEIGHT_INFO' => 'Select the button height in pixels (min: 22 max: 55)',
+);
+
+
+foreach ($lang_array as $key => $val) {
+  defined($key) or define($key, $val);
+}
---
+++ shoproot/lang/german/admin/paypal_module.php
@@ -0,0 +1,97 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: paypal_module.php 16400 2025-04-02 15:29:16Z GTB $
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+
+$lang_array = array(
+  'TEXT_PAYPAL_MODULE_HEADING_TITLE' => 'PayPal Produkte',
+  
+  'TABLE_HEADING_MODULES' => 'Modul',
+  'TABLE_HEADING_FILENAME' => 'Modulname (f&uuml;r internen Gebrauch)',
+  'TABLE_HEADING_SORT_ORDER' => 'Sortierung',
+  'TABLE_HEADING_STATUS' => 'Status',
+  'TABLE_HEADING_ACTION' => 'Aktion',
+
+  'TABLE_HEADING_WALL_STATUS' => 'Auf der Paymentwall anzeigen',
+  'TABLE_HEADING_WALL_DESCRIPTION' => 'Beschreibung',
+  
+  'TEXT_PAYPAL_MODULE_PROFILE' => 'Profil',
+  'TEXT_PAYPAL_NO_PROFILE' => 'kein Webprofil',
+  'TEXT_PAYPAL_STANDARD_PROFILE' => 'Standard Webprofil',
+  
+  'TEXT_PAYPAL_MODULE_LINK_SUCCESS' => 'Link im Checkout',
+  'TEXT_PAYPAL_MODULE_LINK_SUCCESS_INFO' => 'Soll der Zahllink im Checkout angezeigt werden?',
+
+  'TEXT_PAYPAL_MODULE_LINK_ACCOUNT' => 'Link im Account',
+  'TEXT_PAYPAL_MODULE_LINK_ACCOUNT_INFO' => 'Soll der Zahllink im Account angezeigt werden?',
+
+  'TEXT_PAYPAL_MODULE_PRODUCT' => 'Express Button beim Artikel',
+  'TEXT_PAYPAL_MODULE_PRODUCT_INFO' => 'Soll der PayPal Express Button in den Artikel Infos angezeigt werden?',
+
+  'TEXT_PAYPAL_MODULE_CART_BNPL' => 'Sp&auml;ter bezahlen Button im Warenkorb',
+  'TEXT_PAYPAL_MODULE_CART_BNPL_INFO' => 'Soll der PayPal Sp&auml;ter bezahlen Button im Warenkorb angezeigt werden?',
+
+  'TEXT_PAYPAL_MODULE_PRODUCT_BNPL' => 'Sp&auml;ter bezahlen Button beim Artikel',
+  'TEXT_PAYPAL_MODULE_PRODUCT_BNPL_INFO' => 'Soll der PayPal Sp&auml;ter bezahlen Button in den Artikel Infos angezeigt werden?',
+
+  'TEXT_PAYPAL_MODULE_CHECKOUT_BNPL' => 'Sp&auml;ter bezahlen Button im Checkout',
+  'TEXT_PAYPAL_MODULE_CHECKOUT_BNPL_INFO' => 'Soll der PayPal Sp&auml;ter bezahlen Button im Checkout angezeigt werden?',
+
+  'TEXT_PAYPAL_MODULE_BOX_CART' => 'Express Button in Warenkorb Box',
+  'TEXT_PAYPAL_MODULE_BOX_CART_INFO' => 'Soll der PayPal Express Button in Warenkorb Box angezeigt werden?',
+
+  'TEXT_PAYPAL_MODULE_BOX_CART_BNPL' => 'Sp&auml;ter bezahlen Button in Warenkorb Box',
+  'TEXT_PAYPAL_MODULE_BOX_CART_BNPL_INFO' => 'Soll der PayPal Sp&auml;ter bezahlen Button in Warenkorb Box angezeigt werden?',
+
+  'TEXT_PAYPAL_MODULE_SAVE_PAYMENT' => 'Zahlart im Checkout speichern (PayPal Vault)',
+  'TEXT_PAYPAL_MODULE_SAVE_PAYMENT_INFO' => 'Soll die f&uuml;r PayPal verwendete Zahlart f&uuml;r einen schnelleren Checkout bei einer weiteren Bestellung gespeichert werden k&ouml;nnen?',
+
+  'TEXT_PAYPAL_MODULE_OFFER_SAVE_PAYMENT' => 'Gespeicherte Zahlart erlauben (PayPal Vault)',
+  'TEXT_PAYPAL_MODULE_OFFER_SAVE_PAYMENT_INFO' => 'Soll eine f&uuml;r PayPal gespeicherte Zahlart f&uuml;r einen schnelleren Checkout erlaubt werden?',
+
+  'TEXT_PAYPAL_MODULE_ACDC_EXTEND_CARDS' => 'Kreditkarten ohne 3D Secure erlauben',
+  'TEXT_PAYPAL_MODULE_ACDC_EXTEND_CARDS_INFO' => 'Es besteht keine Haftungs&uuml;bernahme bei Zahlungen ohne 3D Secure.',
+
+  'TEXT_PAYPAL_MODULE_USE_TABS' => 'Accordion / Tabs',
+  'TEXT_PAYPAL_MODULE_USE_TABS_INFO' => 'Verwendet das Template Accordion oder Tabs im Checkout?',
+
+  'TEXT_PAYPAL_MODULE_SHIPPING_COST' => 'Vorl&auml;ufige Versandkosten',
+  'TEXT_PAYPAL_MODULE_SHIPPING_COST_INFO' => 'Geben Sie den Betrag f&uuml;r vorl&auml;ufige Versandkosten an.',
+  
+  'TEXT_PAYPAL_MODULE_UPSTREAM_PRODUCT' => 'Finanzierungsbox beim Artikel',
+  'TEXT_PAYPAL_MODULE_UPSTREAM_PRODUCT_INFO' => 'Sollen Details zur Ratenzahlung beim Artikel angezeigt werden?',
+
+  'TEXT_PAYPAL_MODULE_UPSTREAM_CART' => 'Finanzierungsbox im Warenkorb',
+  'TEXT_PAYPAL_MODULE_UPSTREAM_CART_INFO' => 'Sollen Details zur Ratenzahlung im Warenkorb angezeigt werden?',
+
+  'TEXT_PAYPAL_MODULE_UPSTREAM_PAYMENT' => 'Finanzierungsbox im Checkout',
+  'TEXT_PAYPAL_MODULE_UPSTREAM_PAYMENT_INFO' => 'Sollen Details zur Ratenzahlung im Checkout angezeigt werden?',
+
+  'TEXT_PAYPAL_BUTTON_LAYOUT' => 'Button Layout',
+  'TEXT_PAYPAL_BUTTON_LAYOUT_INFO' => 'W&auml;hlen Sie das Button Layout',
+
+  'TEXT_PAYPAL_BUTTON_SHAPE' => 'Button Form',
+  'TEXT_PAYPAL_BUTTON_SHAPE_INFO' => 'W&auml;hlen Sie die Button Form',
+
+  'TEXT_PAYPAL_BUTTON_PRIMARY_COLOR' => 'Button Farbe Paypal',
+  'TEXT_PAYPAL_BUTTON_PRIMARY_COLOR_INFO' => 'W&auml;hlen Sie die Button Farbe',
+
+  'TEXT_PAYPAL_BUTTON_SECONDARY_COLOR' => 'Button Farbe Sp&auml;ter bezahlen',
+  'TEXT_PAYPAL_BUTTON_SECONDARY_COLOR_INFO' => 'W&auml;hlen Sie die Button Farbe',
+
+  'TEXT_PAYPAL_BUTTON_HEIGHT' => 'Button H&ouml;he',
+  'TEXT_PAYPAL_BUTTON_HEIGHT_INFO' => 'W&auml;hlen Sie die Button H&ouml;he in Pixeln (min: 22 max: 55)',
+);
+
+
+foreach ($lang_array as $key => $val) {
+  defined($key) or define($key, $val);
+}
